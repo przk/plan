a4f5429448ae911a41e599620bdb413610ea0488
Przemysław Kuligowski <43840987+przk@users.noreply.github.com>
2020-10-05 17:53:17 +0200

    schedule template

---
+++ schedule.js
@@ -0,0 +1,27 @@
+/*
+szablon
+        {
+            "Name": "", //np. Doktorologia stosowana - wykład
+            "Start": ":00", //np. 9:00
+            "End": ":00", //np. 20:45
+            "Notify": false, //true
+            //"Link": "url",
+        },
+*/
+
+var plan = [
+    [//poniedziałek
+    ],
+    [//wtorek
+    ],
+    [//środa
+    ],
+    [//czwartek
+    ],
+    [//piątek
+    ],
+    [//sobota
+    ],
+    [//niedziela
+    ]
+];
